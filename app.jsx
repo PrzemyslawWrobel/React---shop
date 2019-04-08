92ac0625b2cc0d2e7ccb1fd73af4b031e991deaa
PW <przemek.wrobel@poczta.fm>
2019-04-08 15:34:22 +0200

    r3-2a

--- app.jsx
+++ app.jsx
@@ -2,7 +2,9 @@
 var data = {
 	title: 'Temat Kursu',
 	description: 'Opis kursu...',
-	image: 'http://placehold.it/150x150'
+	image: 'http://placehold.it/150x150',
+	is_new: true,
+	is_promo: true
 }
 
 var course = (
@@ -11,10 +13,11 @@ var course = (
   			<img src={data.image} alt="cover" />
   		</div>
   		<div className="media-body">
-	  		<h3>{data.title}</h3>
+	  		<h3>{data.title} {data.is_new? <span className="label label-default">Nowy!</span> : null}</h3>
   			<p>{data.description}</p>
+  			{data.is_promo? <b>Kurs jest w PROMOCJI!</b> : null }
   		</div>
 	</div>
 )
 
-ReactDOM.render(course, document.getElementById('root'))	
+ReactDOM.render(course, document.getElementById('root'))
