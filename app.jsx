8ea7c13c3d48ee267181fecd7d146ec4a004d64a
PW <przemek.wrobel@poczta.fm>
2019-04-09 09:56:15 +0200

    r3-4

--- app.jsx
+++ app.jsx
@@ -9,12 +9,18 @@ var data = {
 
 var course = (
   	<div className="media">
+		  
+	 	{/* Course media column */}
   		<div className="media-left">
   			<img src={data.image} alt="cover" />
   		</div>
+
+  		{/* Course content column */}
   		<div className="media-body">
 	  		<h3>{data.title} {data.is_new? <span className="label label-default">Nowy!</span> : null}</h3>
   			<p>{data.description}</p>
+
+	  		{/* Promotion */}
   			{data.is_promo? <b>Kurs jest w PROMOCJI!</b> : null }
   		</div>
 	</div>
