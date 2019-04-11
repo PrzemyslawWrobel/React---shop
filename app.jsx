89c6810b9c1baf8a84aea784b5981bbbafb5dae4
PW <przemek.wrobel@poczta.fm>
2019-04-11 13:11:11 +0200

    r4-3

--- app.jsx
+++ app.jsx
@@ -1,14 +1,22 @@
 var CourseMedia = ({data}) => ( <img src={data.image} alt="cover" />)
 
-var NewLabel = ({data}) => ( data.is_new? <span className="label label-default">Nowy!</span> : null)
+var NewLabel = ({data}) => ( data.is_new? <span className="badge badge-secondary">Nowy!</span> : null)
 
 var CoursePromoLabel = ({data}) => ( data.is_promo? <b>Kurs jest w PROMOCJI!</b> : null)
 
+var Button = (props) => ( 
+	<button className="btn btn-outline-secondary">
+		{props.icon? <span className={ "glyphicon glyphicon-" + props.icon }></span> : null}
+		{' '}
+		{props.label}
+	</button>
+)
+
 var CourseActions = ({data}) => (
 	<div className="btn-group pull-right">
-		<button className="btn btn-default">Szczególy kursu</button>
-		<button className="btn btn-default">Dodaj do ulubionych</button>
-		<button className="btn btn-default">Dodaj do koszyka</button>
+		<Button label="Szczególy kursu" />
+		<Button label="Dodaj do ulubionych" icon="star" />
+		<Button label="Dodaj do koszyka" icon="shopping-cart" />
 	</div>
 )
 
@@ -81,4 +89,57 @@ function update(){
 	
 	ReactDOM.render(<CoursesList list={list} />, document.getElementById('root'));
 }
-update();
+update();	
+
+	
+
+
+// Wygląd szablonu przed zmianami
+
+
+// var data = {
+// 	title: 'Temat Kursu',
+// 	description: 'Opis kursu...',
+// 	image: 'http://placehold.it/150x150',
+// 	author: 'Testowy Autor',
+// 	duration: '6 godz',
+// 	is_new: true,
+// 	is_promo: true
+// }
+
+// var course = (
+//   	<div className="media course">
+
+//   		{/* Course media column */}
+//   		<div className="media-left">
+//   			<img src={data.image} alt="cover" />
+//   		</div>
+
+//   		{/* Course content column */}
+//   		<div className="media-body">
+// 	  		<h3>{data.title} {data.is_new? <span className="badge badge-secondary">Nowy!</span> : null}</h3>
+//   			<p>{data.description}</p>
+
+// 	  		{/* Promotion */}
+//   			{data.is_promo? <b>Kurs jest w PROMOCJI!</b> : null }
+//   		</div>
+
+// 	  	{/* Course details column */}
+//   		<div className="media-right">
+// 		  	<table className="table course_details">
+// 		  		<tbody>
+// 			  		<tr>
+// 			  			<th>Autor</th>
+// 			  			<td>{data.author}</td>
+// 		  			</tr>
+// 			  		<tr>
+// 			  			<th>Czas trwania</th>
+// 			  			<td>{data.duration}</td>
+// 		  			</tr>
+// 		  		</tbody>
+// 		  	</table>
+// 	  	</div>
+// 	</div>
+// )
+
+// ReactDOM.render(course, document.getElementById('root'))	
