98ba3559e7216052571b6483d07ba5b963eb40cf
PW <przemek.wrobel@poczta.fm>
2019-04-15 12:49:08 +0200

    rozbicie app na komponenty2

--- app.jsx
+++ app.jsx
@@ -1,143 +1,90 @@
 
-const CoursesList = ({list}) => (
-	<div>
-		<h1> Kursy </h1>
-		<hr />
-		<div>
-			{list.map((data) => <Course data={data} key={data.id} Details={CourseDetails}>
-		  		{/* Promotion */}
-	  			<CoursePromoLabel data={data} />
+var AppState = new StateStore();
 
-		  		{/* Course Actions */}
-				<div className="btn-group pull-right">
-					<Button label="Szczególy kursu" />
-					<StateButton active={AppState.state.favourites_map[data.id]} 
-						onActivate={()=>actions.addFavourite(data.id)} 
-						onDeactivate={()=>actions.removeFavourite(data.id)}  />
-				</div>
-			</Course>)}
-		</div>
-	</div>
-)
-
-const FavouritesCoursesList = ({list}) => (
-	<div>
-		<h1> Ulubione Kursy </h1>
-		<hr />
-		<div>
-			{list.length == 0? <p className="text-center">Brak kursów </p> : null }
-			{list.map((data) => <Course data={data} key={data.id} Details={CourseDetails}>
-		  		{/* Promotion */}
-	  			<CoursePromoLabel data={data} />
-
-		  		{/* Course Actions */}
-				<div className="btn-group pull-right">
-					<Button label="Szczególy kursu" />
-					<StateButton active={AppState.state.favourites_map[data.id]}  
-								onActivate={()=>actions.addFavourite(data.id)} 
-								onDeactivate={()=>actions.removeFavourite(data.id)}  />
-				</div>
-			</Course>)}
-		</div>
-	</div>
-)
-
-
-const StateButton = React.createClass({
-
-	getInitialState: function(){
-		return {
-			active: this.props.active
-		}
-	},
-
-	getDefaultProps: function(){
-		return {
-			active: false,
-			onActivate: function(){},
-			onDeactivate: function(){}
-		}
-	},
-
-	componentWillReceiveProps: function(nextProps){
-		this.setState({
-			active: nextProps.active
-		})
-	},
-
-	setActive: function(){
-		this.setState({
-			active: true
-		})
-		this.props.onActivate()
-	},
-
-	setInactive: function(){
-		this.setState({
-			active: false
-		})
-		this.props.onDeactivate()
-	},
-
-	render: function(){
-		return (this.state.active?
-			<Button label="Usuń z ulubionych" icon="star" onClick={this.setInactive} /> :
-			<Button label="Dodaj do ulubionych" icon="star-empty" onClick={this.setActive} />
-		)
-	}
-
-})
-
-function StateStore(){
-	this.state = {}
-
-	this.dispatchEvents = ()=> {
-		this.callback(this.state)
-	}	
-
-	this.callback = function(){};
-
-	this.addListener = (callback) => {
-		this.callback = callback;
-	}
-
-	this.createAction = function(handler){
-		var state = this.state;
-		return function(){
-			handler.apply(state, arguments);
-
-			AppState.dispatchEvents();
-		}
-	},
-
-	this.createActions = function(handlersMap){
-		var actions = {}
-		for(let name in handlersMap){
-			actions[name] = this.createAction(handlersMap[name])
-		}
-		return actions;
-	}
-}
-
-var AppState = new StateStore()
-AppState.state = {
+AppState.setState({
 	page: 1,
-	courses: courses_data,
+	courses_source: courses_data,
+
 	courses_map: courses_data.reduce((map, course) => {
 		map[course.id] = course;
 		return map;
 	},{}),
-	list: courses_data.slice(0,3),
+	courses_list: courses_data.slice(0,3),
+
 	favourites_list: [],
 	favourites_map: {}
-}
+})
 
-var actions = AppState.createActions({
+const Rating = React.createClass({
+
+	getDefaultProps: function(){
+		return {
+			max: 5,
+			value: 0,
+			onChange: function(){}
+		}
+	},
+
+	componentWillReceiveProps: function(nextProps){
+		if(this.state.rating != nextProps.value){
+			this.setRating(nextProps.value)
+		}
+	},
+
+	getInitialState: function(){
+		return {
+			indicator: this._makeIndicator(this.props.value, this.props.max),
+			rating: this.props.value,
+		}
+	},
+
+	onMouseEnter: function(i){
+		return () => this.setIndicator(i);
+	},
+
+	onMouseLeave: function(i){
+		return () => this.setIndicator(this.state.rating);
+	},
+
+	onClick: function(i){
+		return () => this.setRating(i);
+	},
+
+	setRating: function(rating){
+		this.setState({
+			rating: rating
+		});
+		this.setIndicator(rating);
+		this.props.onChange(rating);
+	},
+
+	setIndicator: function(rating){
+		this.setState({
+			indicator: this._makeIndicator(rating, this.props.max)
+		})
+	},
+
+	_makeIndicator: function(rating, max){
+		return [ ...Array(rating).fill(true), ...Array(max-rating).fill(false) ]
+	},
+
+	render: function(){
+		return <div>
+			{this.state.indicator.map((item, i) => (<span key={i} 
+				className={"glyphicon " + (item? "glyphicon-star" : "glyphicon-star-empty")}
+				onMouseEnter={this.onMouseEnter(i+1)} 
+				onMouseLeave={this.onMouseLeave(i+1)}
+				onClick={this.onClick(i+1)}></span>)) }
+		</div>
+	}
+})
+
+const actions = AppState.createActions({
 	loadMore: function(event){
 		var page = this.page + 1;
 
 		this.page = page,
-		this.list = this.courses.slice(0, this.page * 3)
+		this.courses_list = this.courses_source.slice(0, this.page * 3)
 	},
 	addFavourite: function(id){
 		this.favourites_map[id] = true;
@@ -152,50 +99,5 @@ var actions = AppState.createActions({
 })
 
 
-const App = React.createClass({
-
-	getInitialState: function(){
-		return this.props.store.state;
-	},
-
-	componentDidMount: function(){
-		AppState.addListener((state) => {
-			this.setState({
-				page: state.page,
-				list: state.list,
-				favourites_list: state.favourites_list
-			})
-		})
-	},
-
-	render: function(){
-		return (
-		  <div>
-		    <div className="container">
-		      <div className="row">
-		        <div className="col-xs-12">
-					{/* <ShoppingCartList list={cart_list} /> */}
-					<FavouritesCoursesList list={this.state.favourites_list} />
-					<CoursesList list={this.state.list} />
-		        </div>
-		      </div>
-		      <div className="row">
-		        <div className="col-xs-12">
-		          <hr />
-		          <button className="btn btn-default btn-block" onClick={actions.loadMore}> Pokaż więcej ... </button>
-		        </div>
-		      </div>
-		    </div>
-		    <footer className="footer">
-		      <div className="container">
-		        <p> </p>
-		      </div>
-		    </footer>
-		  </div>
-		)
-	}
-})
-
-
-ReactDOM.render(<App store={AppState} />, document.getElementById('root'));
+ReactDOM.render(<App store={AppState} actions={actions} />, document.getElementById('root'));
 
